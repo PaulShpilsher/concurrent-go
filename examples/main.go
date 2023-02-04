3ff1e15b84fad5847581a068d71fc9d93d91889a
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-02-04 18:49:10 +0300

    refactor

--- examples/main.go
+++ examples/main.go
@@ -16,18 +16,19 @@ func worker(id int) {
 }
 
 func main() {
-	var limit int
-	var tasks int
+	var quota int
+	var numbeOfTasks int
 
-	flag.IntVar(&limit, "limit", 2, "maxiumum number of goroutines allowed to run concurrently")
-	flag.IntVar(&tasks, "tasks", 5, "the number of tasks we need to execute")
+	flag.IntVar(&quota, "limit", 8, "maxiumum number of goroutines allowed to run concurrently")
+	flag.IntVar(&numbeOfTasks, "tasks", 64, "the number of tasks we need to execute")
 	flag.Parse()
 
-	fmt.Printf("Executing %d tasks with concurrency limit %d\n", tasks, limit)
-
 	rand.Seed(time.Now().UnixNano())
-	r, _ := runner.NewConcurrencyRunner(limit)
-	for i := 0; i < tasks; i++ {
+
+	fmt.Printf("Executing %d tasks with concurrency limit %d\n", numbeOfTasks, quota)
+
+	r, _ := runner.NewConcurrencyRunner(quota)
+	for i := 0; i < numbeOfTasks; i++ {
 		id := i
 		r.Run(func() { worker(id) })
 	}
