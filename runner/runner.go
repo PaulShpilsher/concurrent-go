27d15a3727c65e4aef181c1bdb96b1f686d40304
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-02-04 10:54:17 +0300

    implement ExecuteTask

--- runner/runner.go
+++ runner/runner.go
@@ -1,18 +1,53 @@
 package runner
 
+import (
+	"errors"
+	"fmt"
+	"sync/atomic"
+)
+
 const DefaultMaxTasks = 50
 
 type TaskRunner struct {
-	concurrent   chan struct{}
-	runnungTasks uint
+	availableSlots chan struct{}
+	runnungTasks   int32
 }
 
-func NewTaskRunner(limit uint) *TaskRunner {
-	if limit == 0 {
+func NewTaskRunner(limit int) *TaskRunner {
+	if limit <= 0 {
 		limit = DefaultMaxTasks
 	}
-	return &TaskRunner{
-		concurrent:   make(chan struct{}, limit),
-		runnungTasks: 0,
+
+	slots := make(chan struct{}, limit)
+	for i := 0; i < limit; i++ {
+		slots <- struct{}{}
 	}
+
+	return &TaskRunner{
+		availableSlots: slots,
+		runnungTasks:   0,
+	}
+}
+
+func (t *TaskRunner) ExecuteTask(task func()) error {
+	if task == nil {
+		return errors.New("nil task argument")
+	}
+
+	if _, ok := <-t.availableSlots; !ok {
+		return errors.New("channel closed")
+	}
+
+	atomic.AddInt32(&t.runnungTasks, 1)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("Recovered panic in task", r)
+			}
+			atomic.AddInt32(&t.runnungTasks, -1)
+			t.availableSlots <- struct{}{}
+		}()
+
+		task()
+	}()
 }
