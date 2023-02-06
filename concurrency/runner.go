ed1c81bfabcb965dd609bcc42bc512d79ee52cc8
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-02-06 12:14:18 +0300

    implement get number of tasks + refactor

--- concurrency/runner.go
+++ concurrency/runner.go
@@ -1,16 +1,7 @@
 package concurrency
 
-import (
-	"log"
-	"runtime"
-)
-
 type Runner interface {
 	Run(task func()) error
 	WaitAndClose()
-}
-
-func init() {
-	maxWorkers := runtime.GOMAXPROCS(0)
-	log.Println("runtime.GOMAXPROCS", maxWorkers)
+	GetNumberOfRunningTasks() int
 }
