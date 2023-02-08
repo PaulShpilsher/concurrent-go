0630b341565ca04bcb47bc4e04cdfab1f1597930
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-02-08 09:58:44 +0300

    choose runner

--- examples/main.go
+++ examples/main.go
@@ -6,7 +6,9 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/PaulShpilsher/concurrent-go/concurrency/sync/runner"
+	"github.com/PaulShpilsher/concurrent-go/concurrency"
+	channelRunner "github.com/PaulShpilsher/concurrent-go/concurrency/chan/runner"
+	syncRunner "github.com/PaulShpilsher/concurrent-go/concurrency/sync/runner"
 )
 
 func worker(id int) {
@@ -18,15 +20,26 @@ func worker(id int) {
 func main() {
 	var quota int
 	var numbeOfTasks int
+	var kind string
 
+	flag.StringVar(&kind, "kind", "channel", "type of runner: (channel|sync)")
 	flag.IntVar(&quota, "limit", 8, "maxiumum number of goroutines allowed to run concurrently")
 	flag.IntVar(&numbeOfTasks, "tasks", 64, "the number of tasks we need to execute")
 	flag.Parse()
 
-	fmt.Printf("Executing %d tasks with concurrency limit %d\n", numbeOfTasks, quota)
+	var r concurrency.Runner
+	switch kind {
+	case "channel":
+		r = channelRunner.New(quota)
+	case "sync":
+		r = syncRunner.New(quota)
+	default:
+		panic(fmt.Sprintf("Invalid runner kind %s. Allowed channel or runner", kind))
 
-	//r, _ := runner.NewConcurrencyRunner(quota)
-	r := runner.New(quota)
+	}
+
+	fmt.Printf("Executing %d tasks with concurrency limit %d using %s runner\n", numbeOfTasks, quota, kind)
+
 	for i := 0; i < numbeOfTasks; i++ {
 		id := i
 		r.Run(func() { worker(id) })
